a2ec6ccc03beb8e69c2aa441a7863661848aac60
Gumparthy Pavan Kumar <gumparthypk@gmail.com>
2020-10-09 17:59:59 +0530

    Updates

---
+++ precache-manifest.4752832fe636b37af65d3fc79efcb741.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b3cd1e7a1774491d16c1fc76b80eea1",
+    "url": "/ContactManager/index.html"
+  },
+  {
+    "revision": "c8a13955f0c1d7a3ac62",
+    "url": "/ContactManager/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "b87dd025f83c9d47e122",
+    "url": "/ContactManager/static/css/main.3fcd11d6.chunk.css"
+  },
+  {
+    "revision": "c8a13955f0c1d7a3ac62",
+    "url": "/ContactManager/static/js/2.94b948db.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ContactManager/static/js/2.94b948db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b87dd025f83c9d47e122",
+    "url": "/ContactManager/static/js/main.2ddeca34.chunk.js"
+  },
+  {
+    "revision": "22737dce4ea654fb8a7a",
+    "url": "/ContactManager/static/js/runtime-main.78bcf5ee.js"
+  }
+]);
